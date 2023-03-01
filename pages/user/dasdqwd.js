1d7e3d6d893ada87c738b9d8a6cde6a6614cf233
JohanSora <johan.sora@mymarketlogic.com>
2023-03-01 17:21:41 -0500

    add changes

---
+++ pages/user/dasdqwd.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const dasdqwd = () => {
+  return (
+    <>
+      <form id="survey-form">
+        <input type="text" id="name"></input>
+        <input type="email" id="email"></input>
+        <input type="number" id="number"></input>
+      </form>
+    </>
+  );
+};
+
+export default dasdqwd;
