4c860a07f203bc973c3a7670b8cce4a76e079ebc
JohanSora <johan.sora@mymarketlogic.com>
2023-03-09 19:36:16 -0500

    add changes

--- pages/userscreatefunctionFor.js
+++ pages/userscreatefunctionFor.js
@@ -7,7 +7,9 @@ import * as XLSX from "xlsx";
 
 const userscreatefunctionFor = () => {
   const [jsonData, setJsonData] = useState(null);
+  const [jsonData2, setJsonData2] = useState(null);
   const [pos, setPos] = useState([]);
+  const [users, setUsers] = useState([]);
   const token = useSelector((state) => state.user.token);
 
   useEffect(() => {
@@ -21,6 +23,17 @@ const userscreatefunctionFor = () => {
         })
         .then((res) => {
           setPos(res.data);
+        });
+
+      axios
+        .get(`${process.env.BACKURL}/users`, {
+          headers: {
+            "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+            Authorization: `Bearer ${token}`,
+          },
+        })
+        .then((res) => {
+          setUsers(res.data);
         });
     }
   }, [token]);
@@ -39,10 +52,22 @@ const userscreatefunctionFor = () => {
     reader.readAsArrayBuffer(file);
   }
 
+  function xlsxToJson2(e) {
+    const file = e.target.files[0];
+    const reader = new FileReader();
+    reader.onload = () => {
+      const data = new Uint8Array(reader.result);
+      const workbook = XLSX.read(data, { type: "array" });
+      const sheetName = workbook.SheetNames[0];
+      const sheet = workbook.Sheets[sheetName];
+      const json = XLSX.utils.sheet_to_json(sheet);
+      setJsonData2(json);
+    };
+    reader.readAsArrayBuffer(file);
+  }
+
   const handleSubmit = (e) => {
     e.preventDefault();
-
-    console.log;
 
     return jsonData.map((data) => {
       const posFilter = pos.filter((e) => e.description === data.companyName);
@@ -50,7 +75,7 @@ const userscreatefunctionFor = () => {
         posFilter.map((dataPos) => {
           console.log(dataPos);
           console.log({
-            name: `${data.firstName + data.lastName}`,
+            name: `${data.firstName} ${data.lastName}`,
             email: `${data.username}`,
             password: `${data.password}`,
             roleId: `${
@@ -86,8 +111,41 @@ const userscreatefunctionFor = () => {
       }
     });
   };
+  const handleSubmit2 = (e) => {
+    e.preventDefault();
 
-  console.table(jsonData);
+    const usersEmailFile = jsonData2.map((data) => data.email);
+    const usersEmailData = users.map((data) => ({
+      email: data.email,
+      id: data.id,
+    }));
+
+    const usersMatch = usersEmailData.filter((data) =>
+      usersEmailFile.includes(data.email)
+    );
+
+    return usersMatch
+      .map(({ id }) => {
+        axios.patch(
+          `${process.env.BACKURL}/users/${id}`,
+          {
+            policy: true,
+          },
+          {
+            headers: {
+              "Content-Type":
+                "application/x-www-form-urlencoded; charset=UTF-8",
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        );
+      })
+      .then(() => {
+        console.log("all users are updated");
+      });
+  };
+
+  console.table(users);
 
   return (
     <div>
@@ -101,7 +159,20 @@ const userscreatefunctionFor = () => {
           Procesar
         </button>
       </form>
-      {jsonData && <pre>{JSON.stringify(jsonData, null, 2)}</pre>}
+      <form onSubmit={(e) => handleSubmit2(e)}>
+        <input
+          type="file"
+          className="file-input w-full max-w-xs"
+          onChange={xlsxToJson2}
+        />
+        <button type="submit" className="btn btn-primary">
+          Aceptación T&C
+        </button>
+      </form>
+      <div className="flex w-full">
+        {jsonData && <pre>{JSON.stringify(jsonData, null, 2)}</pre>}
+        {jsonData2 && <pre>{JSON.stringify(jsonData2, null, 2)}</pre>}
+      </div>
     </div>
   );
 };
