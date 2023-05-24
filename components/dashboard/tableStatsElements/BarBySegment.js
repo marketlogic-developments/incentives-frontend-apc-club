487645c0efb215a4d4808ae617f0d12c9cfdc2b2
JohanSora <johan.sora@mymarketlogic.com>
2023-05-24 16:15:16 -0500

    add changes

--- components/dashboard/tableStatsElements/BarBySegment.js
+++ components/dashboard/tableStatsElements/BarBySegment.js
@@ -1,103 +1,127 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 const BarBySegment = ({ percentageCC, percentageDC, formatNumber }) => {
+  const ranking = useSelector((state) => state.user.ranking);
   return (
-    <div className="w-full max-sm:mx-auto flex flex-col gap-5 progressiveBar p-6 targetDashboard gap-6">
+    <div
+      className={`w-full max-sm:mx-auto flex flex-col gap-5 progressiveBar p-6 targetDashboard gap-6 ${
+        ranking.length === 0 && "col-span-2"
+      }`}
+    >
       <div>
         <h2 className="font-bold text-xl">Licencias</h2>
         {/* <p></p> */}
       </div>
-      <div className="w-full h-16 flex items-center gap-6 gapBar lg:flex-col xl:flex-row">
-        <div className="flex items-center h-full cct text-center lg:mr-auto">
-          <img
-            src="/assets/dashboard/cc.webp"
-            width={100}
-            className="w-1/2 max-w-[58px]"
-          ></img>
-        </div>
-        <div className="w-full flex flex-col items-center justify-around h-full">
-          <div className="w-full bg-base-200 h-[13px] flex rounded-full overflow-hidden">
-            {percentageCC.map((data) => (
-              <div
-                className={`tooltip ${
-                  data.typeCC === "Teams"
-                    ? "tooltip-primary bg-[#232B2F]"
-                    : data.typeCC === "Enterprise"
-                    ? "tooltip-secondary bg-[#1473E6]"
-                    : "tooltip-success bg-[#009C3B]"
-                } h-full barSales`}
-                data-tip={`$${new Intl.NumberFormat().format(
-                  parseInt(data.sales)
-                )}`}
-                style={{ width: `${data.tablePercentage}%` }}
-              />
-            ))}
+      <div className="flex flex-col gap-6 my-auto">
+        <div className="w-full h-16 flex items-center gap-6 gapBar lg:flex-col xl:flex-row">
+          <div className="flex items-center h-full cct text-center lg:mr-auto">
+            <img
+              src="/assets/dashboard/cc.webp"
+              width={100}
+              className="w-1/2 max-w-[58px]"
+            ></img>
           </div>
-          <div className="w-full flex justify-between">
-            {percentageCC.map((data) => (
-              <p className="text-sm">
-                {data.typeCC} <br />
+          <div className="w-full flex flex-col items-center justify-around h-full">
+            <div className="w-full bg-base-200 h-[13px] flex rounded-full overflow-hidden">
+              {percentageCC.map((data) => (
+                <div
+                  className={`tooltip ${
+                    data.typeCC === "Teams"
+                      ? "tooltip-primary bg-[#232B2F]"
+                      : data.typeCC === "Enterprise"
+                      ? "tooltip-secondary bg-[#1473E6]"
+                      : "tooltip-success bg-[#009C3B]"
+                  } h-full barSales`}
+                  data-tip={`$${new Intl.NumberFormat().format(
+                    parseInt(data.sales)
+                  )}`}
+                  style={{ width: `${data.tablePercentage}%` }}
+                />
+              ))}
+            </div>
+            <div className="w-full grid grid-cols-3">
+              {percentageCC.map((data) => (
                 <p
                   className={`${
                     data.typeCC === "Teams"
-                      ? "text-[#232B2F]"
+                      ? "justify-self-start"
                       : data.typeCC === "Enterprise"
-                      ? "text-[#1473E6]"
-                      : "text-[#009C3B]"
-                  } font-bold`}
+                      ? "justify-self-center"
+                      : "justify-self-end"
+                  } text-sm`}
                 >
-                  {formatNumber(data.sales)}
+                  {data.typeCC} <br />
+                  <p
+                    className={`${
+                      data.typeCC === "Teams"
+                        ? "text-[#232B2F]"
+                        : data.typeCC === "Enterprise"
+                        ? "text-[#1473E6]"
+                        : "text-[#009C3B]"
+                    } font-bold`}
+                  >
+                    {formatNumber(data.sales)}
+                  </p>
                 </p>
-              </p>
-            ))}
+              ))}
+            </div>
           </div>
         </div>
-      </div>
-      <div className="w-full h-16 flex items-center gap-6 gapBar lg:flex-col xl:flex-row">
-        <div className="flex items-center h-full cci text-center lg:mr-auto">
-          <img
-            src="/assets/dashboard/DC.webp"
-            className="w-1/2 max-w-[58px]"
-          ></img>
-        </div>
-        <div className="w-full flex flex-col items-center justify-around h-full">
-          <div className="w-full bg-base-200 h-[13px] flex rounded-full overflow-hidden">
-            {percentageDC.map((data) => (
-              <div
-                className={`tooltip ${
-                  data.typeDC === "Teams"
-                    ? "tooltip-primary bg-[#EB1000]"
-                    : data.typeDC === "Enterprise"
-                    ? "tooltip-secondary bg-[#E9740A]"
-                    : "tooltip-success bg-[#FFCE2E]"
-                } h-full barSales`}
-                data-tip={`$${new Intl.NumberFormat().format(
-                  parseInt(data.sales)
-                )}`}
-                style={{
-                  width: `${data.tablePercentage}%`,
-                  color: "#ffffff",
-                }}
-              />
-            ))}
+        <div className="w-full h-16 flex items-center gap-6 gapBar lg:flex-col xl:flex-row">
+          <div className="flex items-center h-full cci text-center lg:mr-auto">
+            <img
+              src="/assets/dashboard/DC.webp"
+              className="w-1/2 max-w-[58px]"
+            ></img>
           </div>
-          <div className="w-full flex justify-between">
-            {percentageDC.map((data) => (
-              <p className="text-sm">
-                {data.typeDC === "Teams" ? "Acrobat Pro" : data.typeDC} <br />{" "}
+          <div className="w-full flex flex-col items-center justify-around h-full">
+            <div className="w-full bg-base-200 h-[13px] flex rounded-full overflow-hidden">
+              {percentageDC.map((data) => (
+                <div
+                  className={`tooltip ${
+                    data.typeDC === "Teams"
+                      ? "tooltip-primary bg-[#EB1000]"
+                      : data.typeDC === "Enterprise"
+                      ? "tooltip-secondary bg-[#E9740A]"
+                      : "tooltip-success bg-[#FFCE2E]"
+                  } h-full barSales`}
+                  data-tip={`$${new Intl.NumberFormat().format(
+                    parseInt(data.sales)
+                  )}`}
+                  style={{
+                    width: `${data.tablePercentage}%`,
+                    color: "#ffffff",
+                  }}
+                />
+              ))}
+            </div>
+            <div className="w-full grid grid-cols-3">
+              {percentageDC.map((data) => (
                 <p
                   className={`${
                     data.typeDC === "Teams"
-                      ? "text-[#EB1000]"
+                      ? "justify-self-start"
                       : data.typeDC === "Enterprise"
-                      ? "text-[#E9740A]"
-                      : "text-[#FFCE2E]"
-                  } font-bold`}
+                      ? "justify-self-center"
+                      : "justify-self-end"
+                  } text-sm`}
                 >
-                  {formatNumber(data.sales)}
+                  {data.typeDC === "Teams" ? "Acrobat Pro" : data.typeDC} <br />{" "}
+                  <p
+                    className={`${
+                      data.typeDC === "Teams"
+                        ? "text-[#EB1000]"
+                        : data.typeDC === "Enterprise"
+                        ? "text-[#E9740A]"
+                        : "text-[#FFCE2E]"
+                    } font-bold`}
+                  >
+                    {formatNumber(data.sales)}
+                  </p>
                 </p>
-              </p>
-            ))}
+              ))}
+            </div>
           </div>
         </div>
       </div>
--- components/dashboard/tableStatsElements/Meta.js
+++ components/dashboard/tableStatsElements/Meta.js
@@ -8,9 +8,11 @@ const Meta = ({ user, formatNumber, goal, percentageTotal, sales }) => {
           <h2 className="font-bold 2xl:text-xl">Meta partners</h2>
         </div>
         <div className="min-h-[45px] flex items-end">
-          <p className="2xl:!text-3xl lg:!text-xl font-bold text-[#1473E6]">
-            {formatNumber(goal)}
-          </p>
+          {goal !== 0 && (
+            <p className="2xl:!text-3xl lg:!text-xl font-bold text-[#1473E6]">
+              {formatNumber(goal)}
+            </p>
+          )}
         </div>
       </div>
       <div className="h-full w-full flex justify-center ">
@@ -30,7 +32,20 @@ const Meta = ({ user, formatNumber, goal, percentageTotal, sales }) => {
           </div>
         </div>
       </div>
-      <div></div>
+      <div className="w-full flex justify-center metaLi gap-3">
+        <li
+          className="customLi lg:!text-xs xl:!text-base"
+          style={{ "--colorBg": "#2799F6" }}
+        >
+          Alcanzado
+        </li>
+        <li
+          className="customLi lg:!text-xs xl:!text-base"
+          style={{ "--colorBg": "#F2F2F2" }}
+        >
+          Faltante
+        </li>
+      </div>
     </div>
   );
 };
