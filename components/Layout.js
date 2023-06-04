1e535b0bf3581995862be3069e41e5c2abf448d6
Sergio Moran <sergioMoran@gmail.com>
2023-06-04 12:14:12 -0600

    Agregar la foto del usuario  al MenuUser

--- components/Layout.js
+++ components/Layout.js
@@ -47,7 +47,7 @@ const Layout = ({ children }) => {
   const [opened, setOpened] = useState(false);
   const [collapse, setCollapse] = useState(false);
   const [menuUser, setMenuUser] = useState(false);
-
+  console.log(userRedux);
   useEffect(() => {
     if (window.sessionStorage.getItem("infoDt") !== null && userRedux === 0) {
       const userGetData = JSON.parse(window.sessionStorage.getItem("infoDt"));
@@ -1065,10 +1065,20 @@ const Layout = ({ children }) => {
                           className="flex items-center gap-3 bg-[#F5F5F5] rounded-full p-3 min-w-[217px] max-h-[55px] text-xs cursor-pointer"
                           onClick={() => setMenuUser(!menuUser)}
                         >
-                          <div className="user p-2 bg-[#1473E6] rounded-full w-[35px] h-[35px]">
-                            <p className="text-white text-center flex w-full h-full items-center justify-center">
+                          <div className="bg-[#1473E6] rounded-full w-[35px] h-[35px]">
+                            {userRedux.profilePhotoPath === null ||
+                            userRedux.profilePhotoPath === "" ||
+                            userRedux.profilePhotoPath === "noImage" ? (
+                              <p className="text-white text-center flex w-full h-full items-center justify-center">
                               {userRedux?.names[0]}
                             </p>
+                            ) : (
+                              <img
+                                src={userRedux.profilePhotoPath}
+                                className="w-full h-full rounded-full"
+                                alt="Avatar"
+                              />
+                            )}
                           </div>
                           <div className="username">
                             <p className="lg:!text-sm xl:!text-base">
