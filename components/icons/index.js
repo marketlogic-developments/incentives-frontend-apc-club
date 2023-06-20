12b6296a51c9e630dc31834e7d5a97dc4321bccb
Sergio Moran <sergioMoran@gmail.com>
2023-06-19 22:49:43 -0600

    Agregando un nuevo icono a la carpeta de modal

--- components/icons/index.js
+++ components/icons/index.js
@@ -4,4 +4,5 @@ export { default as CloseCircle } from "./modal/CloseCircle";
 export { default as Question } from "./modal/Question";
 export { default as AlertCircule } from "./modal/AlertCircule";
 export { default as ChatBox } from "./modal/ChatBox";
+export { default as BrushOutline } from "./modal/BrushOutline";
 export { default as ArrowDown } from "./Reportes/ArrowDown";
