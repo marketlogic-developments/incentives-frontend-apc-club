b05b4c06f161c106f0b51c618945d4cfd72dfdd5
Sergio Moran <sergioMoran@gmail.com>
2023-06-20 00:10:27 -0600

    Agregando el icono nuevo de SpaklesOuline

--- components/icons/index.js
+++ components/icons/index.js
@@ -5,4 +5,6 @@ export { default as Question } from "./modal/Question";
 export { default as AlertCircule } from "./modal/AlertCircule";
 export { default as ChatBox } from "./modal/ChatBox";
 export { default as BrushOutline } from "./modal/BrushOutline";
+export { default as GiftOutline } from "./modal/GiftOutline";
+export { default as SparklesOutline } from "./modal/SparklesOutline";
 export { default as ArrowDown } from "./Reportes/ArrowDown";
