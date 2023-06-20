fa7974b26ee692bb76750a340bbcd6682232d7c0
Sergio Moran <sergioMoran@gmail.com>
2023-06-19 22:45:11 -0600

    Agregando un nuevo icono

--- components/icons/index.js
+++ components/icons/index.js
@@ -1,4 +1,5 @@
 export { default as HeadSetOuline } from "./menu/HeadSetOuline";
 export { default as CustomIcon } from "./Reportes/CustomIcon";
 export { default as CloseCircle } from "./modal/CloseCircle";
+export { default as Question } from "./modal/Question";
 export { default as ArrowDown } from "./Reportes/ArrowDown";
