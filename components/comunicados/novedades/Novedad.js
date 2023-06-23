14b363abe625600c6708164f1a156cf081976844
Sergio Moran <sergioMoran@gmail.com>
2023-06-23 07:21:38 -0600

    Se creo la carpeta de novedades donde se encuenta el apartado de novedad

---
+++ components/comunicados/novedades/Novedad.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Maintenance from '../../maintenance/Maintenance'
+
+const Novedad = () => {
+  return (
+    <div>
+        <Maintenance />
+    </div>
+  )
+}
+
+export default Novedad
--- components/maintenance/Maintenance.js
+++ components/maintenance/Maintenance.js
@@ -4,7 +4,7 @@ import { LaptopToDo } from '../icons';
 const Maintenance = ({icon = <LaptopToDo />, title = "Estamos trabajando" ,text = 'Próximamente...'}) => {
   return (
     <div className="grid justify-items-center mt-8">
-      <LaptopToDo />
+      {icon}
       <div className="">
         <h1 className="font-bold !text-4xl">{title}</h1>
         <p className="text-xl text-center">{text}</p>
