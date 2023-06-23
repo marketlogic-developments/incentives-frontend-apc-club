b21805a1de0aece469bb744a08218478e27ab23a
Sergio Moran <sergioMoran@gmail.com>
2023-06-23 07:18:59 -0600

    Se creo la carpeta de maintenance que es para poner las imagenes de mantenimiento o de en progreso

--- components/comunicados/marketPlace/MarkertPlace.js
+++ components/comunicados/marketPlace/MarkertPlace.js
@@ -1,14 +1,10 @@
 import React from "react";
-import { LaptopToDo } from "../../icons";
+import Maintenance from "../../maintenance/Maintenance";
 
 const MarkertPlace = () => {
   return (
-    <div className="grid justify-items-center mt-8">
-      <LaptopToDo />
-      <div className="">
-        <h1 className="font-bold !text-4xl">Marketing Place</h1>
-        <p className="text-xl text-center">Proximamente...</p>
-      </div>
+    <div>
+      <Maintenance title="Marketing Place" />
     </div>
   );
 };
