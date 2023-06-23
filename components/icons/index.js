3d69fa9148e9bf143e1c45bcd19d993912f3f3c0
Sergio Moran <sergioMoran@gmail.com>
2023-06-23 07:26:12 -0600

    Se agregaron los iconos al archivo index

--- components/icons/index.js
+++ components/icons/index.js
@@ -2,3 +2,5 @@ export { default as Star } from "./comunicaciones/Star";
 export { default as ArrowDown } from "./Reportes/ArrowDown";
 export { default as SearchIcon } from "./Reportes/Search";
 export { default as LaptopToDo } from "./maintenance/LaptopToDo";
+export { default as CodeRocket } from "./maintenance/CodeRocket";
+export { default as PageFix } from "./maintenance/PageFix";
