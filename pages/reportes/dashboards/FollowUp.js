5331542b60e9d78cafe327a6d26fd4440692ef6a
Sergio Moran <sergioMoran@gmail.com>
2023-06-28 16:45:53 -0600

    Se le agrego el titulo a la Page de Follow up

--- pages/reportes/dashboards/FollowUp.js
+++ pages/reportes/dashboards/FollowUp.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
+import { useTranslation } from "react-i18next";
+import { Maintenance, TitleWithIcon } from "../../../components";
+import { TermsConditions } from "../../../components/icons";
 
 const FollowUp = () => {
+  const [t, i18n] = useTranslation("global");
   return (
-    <div>FollowUp</div>
-  )
-}
+    <div className="mt-8">
+      <div className="grid grid-rows-1">
+        <TitleWithIcon
+          icon={<TermsConditions />}
+          title={t("Reportes.follow_up")}
+        />
+      </div>
+      <Maintenance
+        title="Follow up"
+        text="Follow up goes here!"
+      />
+    </div>
+  );
+};
 
-export default FollowUp
+export default FollowUp;
