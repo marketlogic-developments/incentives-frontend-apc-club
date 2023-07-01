c7ba96cd31542a571366c770b59496b936d73b11
Carlos Vargas <carlos.vargas@mymarketlogic.com>
2023-06-30 18:43:14 -0600

    Update reports and breadcrumbs

--- pages/reportes/dashboards/CustomCare.js
+++ pages/reportes/dashboards/CustomCare.js
@@ -3,6 +3,8 @@ import { useTranslation } from "react-i18next";
 import { Maintenance, TitleWithIcon } from "../../../components";
 import { CodeRocket, CustomIcon } from "../../../components/icons";
 import dynamic from "next/dynamic";
+import { useRouter } from "next/router";
+import { AiOutlineHome, AiOutlineRight } from "react-icons/ai";
 
 const CustomCare = () => {
   const [t, i18n] = useTranslation("global");
@@ -10,6 +12,7 @@ const CustomCare = () => {
   const Zoho = dynamic(() =>
     import("../../../components/embedreports/zoho").then((zoho) => zoho.default)
   );
+  const router = useRouter();
 
   return (
     <div className="mt-8">
@@ -18,6 +21,25 @@ const CustomCare = () => {
           icon={<CustomIcon />}
           title={t("Reportes.custom_care")}
         />
+      </div>
+      <div className="flex w-full items-center gap-4 pt-10 pb-2 pl-0">
+        <AiOutlineHome className="cursor-pointer"
+          onClick={() => {
+          router.push("/dashboard");
+          }}/>
+        <span><AiOutlineRight /></span>
+        <span className="cursor-pointer"
+          onClick={() => {
+          router.push("/reportesDashboard");
+          }}
+        >
+        Reportes
+        </span>
+        <span><AiOutlineRight /></span>
+        <span className="font-bold text-[#1473E6]"
+        >
+        {t("Reportes.custom_care")}
+        </span>
       </div>
       <div className="m-6 flex flex-col gap-16">
         <Zoho />
--- pages/reportes/dashboards/FollowUp.js
+++ pages/reportes/dashboards/FollowUp.js
@@ -3,6 +3,8 @@ import { useTranslation } from "react-i18next";
 import { Maintenance, TitleWithIcon } from "../../../components";
 import { TermsConditions } from "../../../components/icons";
 import dynamic from "next/dynamic";
+import { useRouter } from "next/router";
+import { AiOutlineHome, AiOutlineRight } from "react-icons/ai";
 
 const FollowUp = () => {
 
@@ -10,6 +12,7 @@ const FollowUp = () => {
     import("../../../components/embedreports/PowerBiReport").then((powerBi) => powerBi.default)
   );
   const [t, i18n] = useTranslation("global");
+  const router = useRouter();
   return (
     <div className="mt-8">
       <div className="grid grid-rows-1">
@@ -17,6 +20,25 @@ const FollowUp = () => {
           icon={<TermsConditions />}
           title={t("Reportes.follow_up")}
         />
+      </div>
+      <div className="flex w-full items-center gap-4 pt-10 pb-2 pl-0">
+        <AiOutlineHome className="cursor-pointer"
+          onClick={() => {
+          router.push("/dashboard");
+          }}/>
+        <span><AiOutlineRight /></span>
+        <span className="cursor-pointer"
+          onClick={() => {
+          router.push("/reportesDashboard");
+          }}
+        >
+        Reportes
+        </span>
+        <span><AiOutlineRight /></span>
+        <span className="font-bold text-[#1473E6]"
+        >
+        {t("Reportes.follow_up")}
+        </span>
       </div>
       <div className="m-6 flex flex-col gap-16">
         <PowerBiReport />
