8fd20cc61579238bd78de3e5f92879d46ef97f61
Sergio Moran <sergioMoran@gmail.com>
2023-07-18 23:39:41 -0600

    Colocando un formato a la tabla para cuando se pasen las cantidades de cierto numero

--- components/cardReportes/BarChar.js
+++ components/cardReportes/BarChar.js
@@ -42,6 +42,17 @@ const BarChar = ({
     yAxis: [
       {
         type: "value",
+        axisLabel: {
+          formatter: function (value) {
+            if (value >= 1000000) {
+              return (value / 1000000).toFixed(2) + "M";
+            } else if (value >= 1000) {
+              return (value / 1000).toFixed(2) + "K";
+            } else {
+              return value.toFixed(2);
+            }
+          },
+        },
       },
     ],
     series: [
