ce10ae4ae2fa87f14176e105868fe64d02d9406e
JohanSora <johan.sora@mymarketlogic.com>
2023-07-24 11:56:47 -0500

    adds contenful integration with banners

--- components/dashboard/carouselBanners.js
+++ components/dashboard/carouselBanners.js
@@ -5,16 +5,47 @@ import { useState } from "react";
 import { useTranslation } from "react-i18next";
 import { useSelector } from "react-redux";
 
-const CarouselBanners = () => {
+const CarouselBanners = ({ banners }) => {
   const [t, i18n] = useTranslation("global");
   const route = useRouter();
   const user = useSelector((state) => state.user.user);
   const [counter, setCounter] = useState(0);
-  const typePdf = useMemo(() => {
-    return user.companyId !== null
-      ? ["Promox2.pdf", "Promox2Port.pdf"]
-      : ["Promox2Dist.pdf", "Promox2DistPort.pdf"];
-  }, [i18n]);
+
+  const href = (compOrDist, data) => {
+    if (compOrDist) {
+      const type = data
+        .filter(({ fields }) =>
+          user.companyId !== null
+            ? fields.description.includes("Canales")
+            : fields.description.includes("Distribuidores")
+        )
+        .map(({ fields }) => fields);
+
+      const pdf =
+        i18n.resolvedLanguage === "por"
+          ? type[
+              type.findIndex((element) => element.description.includes("POR"))
+            ]
+          : type[
+              type.findIndex((element) => !element.description.includes("POR"))
+            ];
+
+      return pdf.file.url;
+    }
+
+    const type = data.map(({ fields }) => fields);
+
+    const pdf =
+      i18n.resolvedLanguage === "por"
+        ? type[type.findIndex((element) => element.description.includes("POR"))]
+        : type[
+            type.findIndex((element) => !element.description.includes("POR"))
+          ];
+
+    return pdf.file.url;
+  };
+
+  console.log(banners);
 
   return (
     <Carousel
@@ -22,6 +53,7 @@ const CarouselBanners = () => {
       withIndicators
       onSlideChange={(i) => setCounter(i)}
       initialSlide={counter}
+      loop
       styles={{
         indicator: {
           height: "1rem",
@@ -35,7 +67,49 @@ const CarouselBanners = () => {
         },
       }}
     >
-      <Carousel.Slide>
+      {[...banners]
+        .sort((a, b) => a.order - b.order)
+        .map((data) => {
+          return (
+            <Carousel.Slide>
+              {data.typeRedirection ? (
+                <div
+                  className="w-full flex justify-center cursor-pointer p-[1px]"
+                  onClick={() => route.push(data.redirection)}
+                >
+                  <figure className="w-full">
+                    <img
+                      src={
+                        i18n.resolvedLanguage === "por"
+                          ? data.bannerPor.fields.file.url
+                          : data.bannerEsp.fields.file.url
+                      }
+                      className="bannersImg"
+                    />
+                  </figure>
+                </div>
+              ) : (
+                <a
+                  className="w-full flex justify-center cursor-pointer p-[1px]"
+                  href={href(data.companyAndDistribuitors, data.pdfs)}
+                  target="_blank"
+                >
+                  <figure className="w-full">
+                    <img
+                      src={
+                        i18n.resolvedLanguage === "por"
+                          ? data.bannerPor.fields.file.url
+                          : data.bannerEsp.fields.file.url
+                      }
+                      className="bannersImg"
+                    />
+                  </figure>
+                </a>
+              )}
+            </Carousel.Slide>
+          );
+        })}
+      {/* <Carousel.Slide>
         <a
           className="w-full flex justify-center cursor-pointer p-[1px]"
           href={
@@ -56,24 +130,7 @@ const CarouselBanners = () => {
             />
           </figure>
         </a>
-      </Carousel.Slide>
-      <Carousel.Slide>
-        <div
-          className="w-full flex justify-center cursor-pointer p-[1px]"
-          onClick={() => route.push("/howtowin")}
-        >
-          <figure className="w-full">
-            <img
-              src={
-                i18n.resolvedLanguage === "por"
-                  ? "assets/dashboard/banners/bannerMarketPlacePor.webp"
-                  : "assets/dashboard/banners/bannerMarketPlace.webp"
-              }
-              className="bannersImg"
-            />
-          </figure>
-        </div>
-      </Carousel.Slide>
+      </Carousel.Slide> */}
     </Carousel>
   );
 };
