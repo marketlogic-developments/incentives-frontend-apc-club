fd33017e3d7fab1999bdb08d48386fdfdace294e
JohanSora <johan.sora@mymarketlogic.com>
2023-07-31 17:02:39 -0500

    add changes

--- components/dashboard/TableTopsRanking.js
+++ components/dashboard/TableTopsRanking.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import NoDataRanking from "./tableStatsElements/NoDataRanking";
 import { useTranslation } from "react-i18next";
@@ -8,6 +8,8 @@ import BtnWithImage from "../cardReportes/BtnWithImage";
 import jsonexport from "jsonexport";
 import { saveAs } from "file-saver";
 import SelectInputValue from "../inputs/SelectInputValue";
+import { comment } from "postcss";
+import axios from "axios";
 
 const TableTopsRanking = ({
   containerStyles = "",
@@ -18,12 +20,15 @@ const TableTopsRanking = ({
 }) => {
   const ranking = useSelector((state) => state.user.ranking);
   const user = useSelector((state) => state.user.user);
+  const token = useSelector((state) => state.user.token);
+  const [allCompanies, setAllCompanies] = useState([]);
+  const [regions, setRegions] = useState([]);
+  const [level, setLevel] = useState([]);
   const [t, i18n] = useTranslation("global");
-  const filters = {
+  const [filters, setFilters] = useState({
     company: "",
     region: "",
-    level: "",
-  };
+  });
 
   /* Download */
   const importFile = (data) => {
@@ -37,21 +42,77 @@ const TableTopsRanking = ({
     });
   };
 
+  useEffect(() => {
+    const companies = axios.get(
+      `${process.env.NEXT_PUBLIC_BACKEND_URL}/companies`,
+      {
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+
+    const distribution = axios.get(
+      `${process.env.NEXT_PUBLIC_BACKEND_URL}/distribution-channel`,
+      {
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+
+    Promise.allSettled([companies, distribution])
+      .then((res) => {
+        const arrMap1 = res[0].value.data.map(({ name }) => ({ name: name }));
+        const arrMap2 = res[1].value.data.map(({ nameDist }) => ({
+          nameDist: nameDist,
+        }));
+
+        setAllCompanies([...arrMap1, ...arrMap2].sort());
+      })
+      .finally(() => {
+        const regions = [...new Set(ranking.map(({ region }) => region))];
+
+        setRegions(regions);
+      });
+  }, [token]);
+
+  const handleFilter = (name, info) => {
+    const { [name]: extractedValue, ...rest } = filters;
+    const keys = Object.keys(rest);
+    console.log(keys);
+    setFilters({ [keys[0]]: "", [name]: info });
+  };
+
   return (
     <div className="grid w-full">
       <div className="flex justify-between items-center">
         <div>
           <h2 className="!text-xl font-bold">{t("dashboard.topUsuarios")}</h2>
         </div>
-        <div className="cursor-pointer flex gap-3 items-center invisible">
+        <div className="cursor-pointer flex gap-3 items-center">
           <div className="sm:w-[90%] w-[60%]">
             <SelectInputValue
               placeholder={"Company Name"}
-              // value={filters.company}
-              // // data={filteredUsers.map(({ company_name }) => company_name)}
+              value={filters.company}
+              data={allCompanies
+                .map(({ name, nameDist }) => name || nameDist)
+                .sort()}
               icon={<ArrowDown />}
-              // onChange={handleFilters}
+              onChange={handleFilter}
               name={"company"}
+            />
+          </div>
+          <div className="sm:w-[90%] w-[60%]">
+            <SelectInputValue
+              placeholder={"Región"}
+              value={filters.region}
+              data={regions.map((i) => i)}
+              icon={<ArrowDown />}
+              onChange={handleFilter}
+              name={"region"}
             />
           </div>
         </div>
@@ -98,16 +159,28 @@ const TableTopsRanking = ({
           </thead>
           <tbody>
             {ranking.length !== 0 &&
-              ranking.slice(0, 5).map((data, index) => (
-                <tr>
-                  <td className="p-2 text-xl font-bold text-left">
-                    #{data.ranking}
-                  </td>
-                  <td className="text-left">{data.names}</td>
-                  <td className="text-left">{data.email}</td>
-                  <td className="text-left">{data.region}</td>
-                </tr>
-              ))}
+              [...ranking]
+                .filter((i) => {
+                  if (filters.region.length > 0) {
+                    return i.region === filters.region;
+                  }
+                  if (filters.company.length > 0) {
+                    return i.company === filters.company;
+                  }
+
+                  return i;
+                })
+                .slice(0, 5)
+                .map((data, index) => (
+                  <tr>
+                    <td className="p-2 text-xl font-bold text-left">
+                      #{data.ranking}
+                    </td>
+                    <td className="text-left">{data.names}</td>
+                    <td className="text-left">{data.email}</td>
+                    <td className="text-left">{data.region}</td>
+                  </tr>
+                ))}
           </tbody>
         </table>
         <div className="mb-6">{ranking.length === 0 && <NoDataRanking />}</div>
