ca57edca701eb2b9ef4dad42f99bd6dc43ef5ba7
Sergio Moran <sfmorancob@gmail.com>
2023-08-14 14:27:51 -0600

    se agrego la traduccion de summary al traductor es

--- pages/reportes/dashboards/Summary.js
+++ pages/reportes/dashboards/Summary.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import { TitleWithIcon } from '../../../components'
 
 const Summary = () => {
   return (
-    <div>Summary</div>
+    <div className="mt-8">
+         <div className="pt-2 grid items-center grid-rows-1 gap-3">
+        <TitleWithIcon icon={<User />} title={t("Reportes.user_performance")} />
+      </div>
+    </div>
   )
 }
 
