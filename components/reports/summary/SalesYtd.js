98cf2fc12a0e0493b49ed1bb05c83c0183ce368a
Sergio Moran <sfmorancob@gmail.com>
2023-08-16 10:13:19 -0600

    Advance to SalesYtd

--- components/reports/summary/SalesYtd.js
+++ components/reports/summary/SalesYtd.js
@@ -9,57 +9,58 @@ import BtnWithImage from "../../cardReportes/BtnWithImage";
 const SalesYtd = () => {
   const [t, i18n] = useTranslation("global");
   return (
-    <div className="pt-2 grid items-center sm:grid-cols-6 grid-rows-1 gap-3">
-      <SelectInputValue
-        placeholder={"Year"}
-        /* value={selectOne}
+    <div>
+      <div className="pt-2 grid items-center sm:grid-cols-6 grid-rows-1 gap-3">
+        <SelectInputValue
+          placeholder={"Year"}
+          /* value={selectOne}
           data={dataSelectOne} */
-        icon={<ArrowDown />}
-        searchable={true}
-        /* onChange={handleSelectOneChange} */
-        name={"year"}
-      />
-      <SelectInputValue
-        placeholder={"Quarter"}
-        /* value={selectOne}
-          data={dataSelectOne} */
-        icon={<ArrowDown />}
-        searchable={true}
-        /* onChange={handleSelectOneChange} */
-        name={"quarter"}
-      />
-      <SelectInputValue
-        placeholder={"Month"}
-        /* value={selectOne}
-          data={dataSelectOne} */
-        icon={<ArrowDown />}
-        searchable={true}
-        /* onChange={handleSelectOneChange} */
-        name={"month"}
-      />
-      <SelectInputValue
-        placeholder={"Region"}
-        /* value={selectOne}
-          data={dataSelectOne} */
-        icon={<ArrowDown />}
-        searchable={true}
-        /* onChange={handleSelectOneChange} */
-        name={"region"}
-      />
-      <BtnWithImage
-        text={t("Más filtros")}
-        icon={<Filter />}
-        styles={
-          "bg-white btn-sm !text-blue-500 hover:bg-white border-none mt-2"
-        }
-        /* onClick={() => importFile(data)} */
-      />
-      <div className="w-4/5 justify-end">
-      <BtnFilter
-        text={t("Reportes.limpiar_filtros")}
-        styles="bg-white !text-blue-500 sm:!text-base hover:bg-white border-none hover:border-none m-1"
-        /* onClick={clearSelects} */
-      />
+          icon={<ArrowDown />}
+          searchable={true}
+          /* onChange={handleSelectOneChange} */
+          name={"year"}
+        />
+        <SelectInputValue
+          placeholder={"Quarter"}
+          icon={<ArrowDown />}
+          searchable={true}
+          name={"quarter"}
+        />
+        <SelectInputValue
+          placeholder={"Month"}
+          icon={<ArrowDown />}
+          searchable={true}
+          name={"month"}
+        />
+        <SelectInputValue
+          placeholder={"Region"}
+          icon={<ArrowDown />}
+          searchable={true}
+          name={"region"}
+        />
+        <BtnWithImage
+          text={t("Más filtros")}
+          icon={<Filter />}
+          styles={
+            "bg-white btn-sm !text-blue-500 hover:bg-white border-none mt-2"
+          }
+          /* onClick={() => importFile(data)} */
+        />
+        <div className="w-4/5 justify-end">
+          <BtnFilter
+            text={t("Reportes.limpiar_filtros")}
+            styles="bg-white !text-gray-400 sm:!text-base hover:bg-white hover:!text-blue-500 border-none hover:border-none m-1"
+            /* onClick={clearSelects} */
+          />
+        </div>
+      </div>
+      <div className="p-3">
+        <h1 className="text-black font-bold">Total Sales vs Goals</h1>
+      </div>
+      <div className="grid sm:grid-cols-3 grid-cols-1 divide-x">
+        <div>1</div>
+        <div>2</div>
+        <div>3</div>
       </div>
     </div>
   );
