67887fe38e8c7897934ec7e8759053ce357e8200
Sergio Moran <sfmorancob@gmail.com>
2023-08-16 10:14:51 -0600

    Se agrego el icono de circle

--- components/icons/index.js
+++ components/icons/index.js
@@ -1,4 +1,3 @@
-
 export { default as Star } from "./comunicaciones/Star";
 export { default as LaptopToDo } from "./maintenance/LaptopToDo";
 export { default as CodeRocket } from "./maintenance/CodeRocket";
@@ -34,3 +33,4 @@ export { default as ShoppingCard } from "./navbar/ShoppingCard";
 export { default as Megaphone } from "./navbar/Megaphone";
 export { default as Camera } from "./perfil/Camera";
 export { default as Filter } from "./Reportes/Filter";
+export { default as Circle } from "./Reportes/Circle";
