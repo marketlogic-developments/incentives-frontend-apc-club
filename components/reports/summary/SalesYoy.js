224de3a7b97218d7b0afb3fdf44abebf2c9a6b85
Sergio Moran <sfmorancob@gmail.com>
2023-08-17 09:33:19 -0600

    Se agrego la fila de filtros

--- components/reports/summary/SalesYoy.js
+++ components/reports/summary/SalesYoy.js
@@ -1,9 +1,147 @@
-import React from 'react'
+import React from "react";
+import SelectInputValue from "../../inputs/SelectInputValue";
+import { ArrowDown } from "../../icons";
+import BtnFilter from "../../cardReportes/BtnFilter";
+import SalesYtdMultiselectModal from "../../ModalStateProducts/SalesYtdMultiselectModal";
+import { useTranslation } from "react-i18next";
 
 const SalesYoy = () => {
+  /* Variable and const */
+  const [t, i18n] = useTranslation("global");
+  const multiSelect = [
+    {
+      placeholder: "Year",
+      value: [],
+      dataSelect: [],
+      searchable: true,
+      icon: <ArrowDown />,
+      name: "year",
+    },
+    {
+      placeholder: "Quater",
+      value: [],
+      dataSelect: [],
+      searchable: true,
+      icon: <ArrowDown />,
+      name: "quater",
+    },
+    {
+      placeholder: "Month",
+      value: [],
+      dataSelect: [],
+      searchable: true,
+      icon: <ArrowDown />,
+      name: "Month",
+    },
+    {
+      placeholder: "Region",
+      value: [],
+      dataSelect: [],
+      searchable: true,
+      icon: <ArrowDown />,
+      name: "region",
+    },
+    {
+      placeholder: "Country",
+      value: [],
+      dataSelect: [],
+      searchable: true,
+      icon: <ArrowDown />,
+      name: "country",
+    },
+    {
+      placeholder: "Partner level",
+      value: [],
+      dataSelect: [],
+      searchable: true,
+      icon: "",
+      name: "partner_level",
+    },
+    {
+      placeholder: "Partner",
+      value: [],
+      dataSelect: [],
+      searchable: true,
+      icon: "",
+      name: "partner",
+    },
+    {
+      placeholder: "Market segment",
+      value: [],
+      dataSelect: [],
+      searchable: true,
+      icon: "",
+      name: "market_segment",
+    },
+    {
+      placeholder: "Business unit",
+      value: [],
+      dataSelect: [],
+      searchable: true,
+      icon: "",
+      name: "business_unit",
+    },
+    {
+      placeholder: "Business type",
+      value: [],
+      dataSelect: [],
+      searchable: true,
+      icon: "",
+      name: "business_type",
+    },
+    {
+      placeholder: "Licensing type",
+      value: [],
+      dataSelect: [],
+      searchable: true,
+      icon: "",
+      name: "licensiong",
+    },
+  ];
   return (
-    <div>SalesYoy</div>
-  )
-}
+    <div className="m-5">
+      <div className="pt-2 grid items-center sm:grid-cols-6 grid-rows-1 gap-3">
+        <SelectInputValue
+          placeholder={"Year"}
+          /* value={selectOne}
+          data={dataSelectOne} */
+          icon={<ArrowDown />}
+          searchable={true}
+          /* onChange={handleSelectOneChange} */
+          name={"year"}
+        />
+        <SelectInputValue
+          placeholder={"Quarter"}
+          icon={<ArrowDown />}
+          searchable={true}
+          name={"quarter"}
+        />
+        <SelectInputValue
+          placeholder={"Month"}
+          icon={<ArrowDown />}
+          searchable={true}
+          name={"month"}
+        />
+        <SelectInputValue
+          placeholder={"Region"}
+          icon={<ArrowDown />}
+          searchable={true}
+          name={"region"}
+        />
+        <SalesYtdMultiselectModal
+          title={<p className="text-black font-bold text-lg">Filtrar por</p>}
+          datas={multiSelect}
+        />
+        <div className="w-4/5 justify-end">
+          <BtnFilter
+            text={t("Reportes.limpiar_filtros")}
+            styles="bg-white !text-gray-400 sm:!text-base hover:bg-white hover:!text-blue-500 border-none hover:border-none m-1"
+            /* onClick={clearSelects} */
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default SalesYoy
+export default SalesYoy;
