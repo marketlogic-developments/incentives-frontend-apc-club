7a3560c9ac54dc6d17de0285db32560b794e67bb
Sergio Moran <sfmorancob@gmail.com>
2023-08-25 10:20:37 -0600

    Se cambio el orden de los archivos y de los exports

--- components/index.js
+++ components/index.js
@@ -25,5 +25,4 @@ export { default as BasicBarChart } from "./charts/BasicBarChart";
 export { default as PieChart } from "./charts/PieChart";
 export { default as SalesYoy } from "./reports/summary/SalesYoy";
 export { default as HorizontalDoubleChart } from "./charts/HorizontalDoubleChart";
-export { default as DigipoinstPerformance } from "./reports/summary/DigipoinstPerformance";
 export { default as Organization } from "./reports/summary/Organization";
--- pages/reportes/dashboards/Summary.js
+++ pages/reportes/dashboards/Summary.js
@@ -1,15 +1,10 @@
 import React, { useMemo, useState } from "react";
-import {
-  DigipoinstPerformance,
-  Organization,
-  SalesYoy,
-  TitleWithIcon,
-} from "../../../components";
+import { Organization, SalesYoy, TitleWithIcon } from "../../../components";
 import { useTranslation } from "react-i18next";
 import { AiOutlineHome, AiOutlineRight } from "react-icons/ai";
 import { useRouter } from "next/router";
 import ButtonBgOut from "../../../components/buttons/ButtonBgOut";
-import { SalesYtd } from "../../../components/reports";
+import { SalesYtd, DigipoinstPerformance } from "../../../components/reports";
 
 const Summary = () => {
   /* Variables and conts */
@@ -80,7 +75,7 @@ const Summary = () => {
               : "hover:bg-red-100 hover:!text-red-500 hover:!text-sm"
           }`}
           onClick={() => setContent(t("SalesYoy"))}
-        />
+        /> */}
         <ButtonBgOut
           title={t("Reportes.digipoints_performance")}
           styles={`${
@@ -90,7 +85,7 @@ const Summary = () => {
           }`}
           onClick={() => setContent(t("DigipoinstPerformance"))}
         />
-        <ButtonBgOut
+        {/* <ButtonBgOut
           title={t("Reportes.organization")}
           styles={`${
             content === "Organization"
