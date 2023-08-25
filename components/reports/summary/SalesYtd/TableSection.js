c392ae016f30ff221caf0d0cec93ced8e5bfa6c1
Sergio Moran <sfmorancob@gmail.com>
2023-08-25 07:43:01 -0600

    En el nuevo componente que forma el Table Section se coloco el formato de texto

--- components/reports/summary/SalesYtd/TableSection.js
+++ components/reports/summary/SalesYtd/TableSection.js
@@ -1,63 +1,61 @@
-import React from 'react'
-import SortedTable from '../../../table/SortedTable'
+import React from "react";
+import SortedTable from "../../../table/SortedTable";
 
-const TableSection = ({dataTable}) => {
+const TableSection = ({ dataTable }) => {
   return (
     <SortedTable
-            containerStyles={
-              "mt-4 !rounded-tl-lg !rounded-tr-lg max-h-max !w-full"
-            }
-            tableStyles={"table-zebra !text-sm"}
-            colStyles={"p-2"}
-            thStyles={"sticky text-white"}
-            cols={[
-              {
-                rowStyles: "",
-                sort: false,
-                symbol: "",
-                identity: "company_type",
-                columnName: "Segmento",
-              },
-              {
-                rowStyles: "",
-                sort: false,
-                symbol: "USD",
-                identity: "sales_education_cc",
-                columnName: "CC Education",
-              },
-              {
-                rowStyles: "",
-                sort: false,
-                symbol: "USD",
-                identity: "sales_education_dc",
-                columnName: "DC Education",
-              },
-              {
-                symbol: "USD",
-                identity: "sales_enterprise_cc",
-                columnName: "CC Enterprise",
-              },
-              {
-                symbol: "USD",
-                identity: "sales_enterprise_dc",
-                columnName: "DC Acrobat Pro",
-              },
-              {
-                symbol: "USD",
-                identity: "sales_teams_cc",
-                columnName: "CC Teams",
-              },
-              {
-                symbol: "USD",
-                identity: "sales_acrobat_pro_dc",
-                columnName: "DC Acrobat Pro",
-              },
-            ]}
-            generalRowStyles={"text-left py-3 mx-7"}
-            currentItems={dataTable}
-            sumColum={true}
-          />
-  )
-}
+      containerStyles={"mt-4 !rounded-tl-lg !rounded-tr-lg max-h-max !w-full"}
+      tableStyles={"table-zebra !text-sm"}
+      colStyles={"p-2"}
+      thStyles={"sticky text-white"}
+      cols={[
+        {
+          rowStyles: "",
+          sort: false,
+          symbol: "",
+          identity: "company_type",
+          columnName: "Segmento",
+        },
+        {
+          rowStyles: "",
+          sort: false,
+          symbol: "USD",
+          identity: "sales_education_cc",
+          columnName: "CC Education",
+        },
+        {
+          rowStyles: "",
+          sort: false,
+          symbol: "USD",
+          identity: "sales_education_dc",
+          columnName: "DC Education",
+        },
+        {
+          symbol: "USD",
+          identity: "sales_enterprise_cc",
+          columnName: "CC Enterprise",
+        },
+        {
+          symbol: "USD",
+          identity: "sales_enterprise_dc",
+          columnName: "DC Acrobat Pro",
+        },
+        {
+          symbol: "USD",
+          identity: "sales_teams_cc",
+          columnName: "CC Teams",
+        },
+        {
+          symbol: "USD",
+          identity: "sales_acrobat_pro_dc",
+          columnName: "DC Acrobat Pro",
+        },
+      ]}
+      generalRowStyles={"text-left py-3 mx-7"}
+      currentItems={dataTable}
+      sumColum={true}
+    />
+  );
+};
 
-export default TableSection
+export default TableSection;
