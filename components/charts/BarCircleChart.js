e4296d8534290231662ce25a84d29641fe407e03
Sergio Moran <sfmorancob@gmail.com>
2023-08-27 12:12:50 -0600

    Se valido CERTIFIE

--- components/charts/BarCircleChart.js
+++ components/charts/BarCircleChart.js
@@ -1,3 +1,4 @@
+import { Tooltip } from "@mantine/core";
 import React from "react";
 import { useTranslation } from "react-i18next";
 
@@ -6,40 +7,53 @@ const BarCircleChart = ({ datas }) => {
   return (
     <div className="flex flex-col w-full p-4 gap-4 targetDashboard">
       <div className="flex justify-between">
-        <h3 className="font-bold xl:!text-xl lg:!text-sm">
+        <h3 className="font-bold xl:!text-4xl lg:!text-sm">
           Levels vs Sales goal
         </h3>
       </div>
       <div className="grid grid-flow-row h-full gap-3">
-        {datas.map((data, index) => (
-          <div className="flex flex-col gap-2" key={index}>
-            <div className="flex w-full justify-between">
-              <p className="lg:!text-xs xl:!text-sm font-bold">{data.level}</p>
-              <p className="!text-sm">
-                {Number(
-                  (data.total_revenue * 100) /
-                    (data.total_expected_revenue === '0.00'
-                      ? 1
-                      : data.total_expected_revenue)
-                ).toFixed(0)}{" "}
-                %
-              </p>
-            </div>
-            <div className="w-full bg-base-200 h-[13px] flex rounded-full overflow-hidden">
-              <span
-                className={`bg-[${data?.color}] h-full rounded-full`}
-                style={{
-                  width: `${Number(
+        {datas.map((data, index) =>
+          data.level !== "CERTIFIED" ? (
+            <div className="flex flex-col gap-2" key={index}>
+              <div className="flex w-full justify-between">
+                <p className="lg:!text-xs xl:!text-sm font-bold">
+                  {data.level}
+                </p>
+                <p className="!text-sm">
+                  {Number(
                     (data.total_revenue * 100) /
-                      (data.total_expected_revenue === '0.00'
+                      (data.total_expected_revenue === "0.00"
                         ? 1
                         : data.total_expected_revenue)
-                  ).toFixed(0)}%`,
-                }}
-              />
+                  ).toFixed(0)}{" "}
+                  %
+                </p>
+              </div>
+              <Tooltip
+                label={`${Number(
+                  (data.total_revenue * 100) /
+                    (data.total_expected_revenue === "0.00"
+                      ? 1
+                      : data.total_expected_revenue)
+                ).toFixed(2)}%`}
+              >
+                <div className="w-full bg-base-200 h-[13px] flex rounded-full overflow-hidden">
+                  <span
+                    className={`bg-[${data?.color}] h-full rounded-full`}
+                    style={{
+                      width: `${Number(
+                        (data.total_revenue * 100) /
+                          (data.total_expected_revenue === "0.00"
+                            ? 1
+                            : data.total_expected_revenue)
+                      ).toFixed(0)}%`,
+                    }}
+                  />
+                </div>
+              </Tooltip>
             </div>
-          </div>
-        ))}
+          ) : null
+        )}
       </div>
     </div>
   );
