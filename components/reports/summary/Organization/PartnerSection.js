b0d9c6a891a31d33463a3f5c1e0aaab9e49784b2
Sergio Moran <sfmorancob@gmail.com>
2023-08-31 18:52:01 -0600

    Se agregaron y validaron datos tanto para el sorted table como para la grafica de partner

--- components/reports/summary/Organization/PartnerSection.js
+++ components/reports/summary/Organization/PartnerSection.js
@@ -1,15 +1,39 @@
-import React from "react";
+import React, { useMemo, useState } from "react";
 import SortedTable from "../../../table/SortedTable";
 import { SegmentedControl } from "@mantine/core";
 
 const PartnerSection = ({ isDataLoading, partner }) => {
+  const itemsPerPage = 10;
+  const [itemOffset, setItemOffset] = useState(0);
+
+  const currentItems = useMemo(() => {
+    const endOffset = itemOffset + itemsPerPage;
+
+    if (partner?.length === 1) {
+      return partner;
+    }
+
+    return partner?.slice(itemOffset, endOffset);
+  }, [itemOffset, partner]);
+    /* Paginate */
+    const pageCount = useMemo(
+      () => Math.ceil(partner?.length / itemsPerPage),
+      [itemsPerPage]
+    );
+
+    const handlePageClick = (event) => {
+      const newOffset = (event.selected * itemsPerPage);
+  
+      setItemOffset(newOffset);
+    };
+
   return (
     <>
       <div className="grid sm:grid-cols-2 grid-rows-1 gap-5 pt-10">
         <p className="text-black font-bold text-3xl">
           Partner that grew and decreased +-50
         </p>
-        <SegmentedControl
+        {/* <SegmentedControl
           data={[
             { value: "preview", label: "Who increased +50%" },
             { value: "code", label: "Who decreased -50%" },
@@ -17,7 +41,7 @@ const PartnerSection = ({ isDataLoading, partner }) => {
           color="dark"
           fullWidth 
           radius={'lg'}
-        />
+        /> */}
       </div>
       <div className="justify-items-center pt-5">
         {!isDataLoading && <div className="lds-dual-ring"></div>}
@@ -59,13 +83,13 @@ const PartnerSection = ({ isDataLoading, partner }) => {
               },
               {
                 symbol: "USD",
-                identity: "growth",
+                identity: "expected_revenue ",
                 columnName: "Sales LY",
               },
               {
                 symbol: "%",
                 identity: "growth",
-                columnName: "Growth",
+                columnName: "Total % effectiveness",
               },
               {
                 symbol: "USD",
@@ -89,8 +113,11 @@ const PartnerSection = ({ isDataLoading, partner }) => {
               },
             ]}
             generalRowStyles={"text-left py-3 mx-7"}
-            currentItems={partner}
+            paginate={true}
+            currentItems={currentItems}
             sumColum={true}
+            pageCount={pageCount}
+            handlePageClick={handlePageClick}
             totalTableStyles={"text-green-400 text-left text-lg font-bold"}
           />
         )}
