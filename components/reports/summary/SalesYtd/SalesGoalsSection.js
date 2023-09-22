40113bcf2479b0fcc2bf8c4ddead6e0538a1cc80
Sergio Moran <sfmorancob@gmail.com>
2023-09-22 11:11:21 -0600

    Responsividad SalesGoalsSection

--- components/reports/summary/SalesYtd/SalesGoalsSection.js
+++ components/reports/summary/SalesYtd/SalesGoalsSection.js
@@ -7,23 +7,23 @@ const SalesGoalsSection = ({ dataLoaded, totalSaleGoal }) => {
       <div className="p-3">
         <h1 className="text-black font-bold">Total Sales vs Goals</h1>
       </div>
-      <div className="grid sm:grid-cols-3 grid-cols-1 divide-x">
+      <div className="grid sm:grid-cols-3 grid-rows-1 sm:divide-x gap-2">
         {!dataLoaded && <div className="lds-dual-ring"></div>}
-        <div className="flex justify-center gap-3">
+        <div className="flex sm:justify-center justify-start gap-3">
           <Circle />
           <div className="grid">
             <h3 className="text-gray-400 font-bold">Expected</h3>
             <h1 className="text-black font-bold">$ {totalSaleGoal.expected}</h1>
           </div>
         </div>
-        <div className="flex justify-center gap-3">
+        <div className="flex sm:justify-center justify-start gap-3">
           <Check />
           <div className="grid">
             <h3 className="text-gray-400 font-bold">Reached</h3>
             <h1 className="text-black font-bold">$ {totalSaleGoal.reached}</h1>
           </div>
         </div>
-        <div className="flex justify-center gap-3">
+        <div className="flex sm:justify-center justify-start gap-3">
           <Circle />
           <div className="grid">
             <h3 className="text-gray-400 font-bold">Progress</h3>
