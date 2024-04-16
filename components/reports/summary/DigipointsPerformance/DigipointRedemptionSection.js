d7c7d2ebc747d653b98fb194c720f0cda28a841e
JohanSora <johan.sora@mymarketlogic.com>
2024-04-16 14:42:25 -0500

    add changes

--- components/reports/summary/DigipointsPerformance/DigipointRedemptionSection.js
+++ components/reports/summary/DigipointsPerformance/DigipointRedemptionSection.js
@@ -22,11 +22,16 @@ const DigipointRedemptionSection = ({
     ),
   ].sort((a, b) => a - b);
 
+  const DigiStatus = dataDigStatus.map((data) => ({
+    ...data,
+    value: Number(data.value),
+  }));
+
   return (
     <>
       <CardChart title={"DigiPoints by status"} paragraph="">
         {!isDataReady && <div className="lds-dual-ring"></div>}
-        {isDataReady && <HorizontalBar datas={dataDigStatus} symbol="" />}
+        {isDataReady && <HorizontalBar datas={DigiStatus} symbol="" />}
       </CardChart>
       <Suspense fallback={<div className="lds-dual-ring"></div>}>
         <CardChart title={"Redemptions by region and amound"} paragraph="">
