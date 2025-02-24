86ef36c3eef11c6a7de82d0460cb38156b8dce50
ogarcia-market <60816648+ogarcia-dev@users.noreply.github.com>
2025-02-24 10:56:37 -0500

    Update connectapi.service.ts

--- services/connectapi.service.ts
+++ services/connectapi.service.ts
@@ -5,7 +5,7 @@ const createInstance = (): AxiosInstance => {
         baseURL: `${process.env.NEXT_PUBLIC_BACKEND_URL}`,
         headers: { 
             "Content-Type": "application/json",
-            "Access-Control-Allow-Origin": "*",  
+            "Access-Control-Allow-Origin": "*" 
         }
     });
 
