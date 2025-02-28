f873debc65f1a47deb86c2d68fdc193a2606d0f8
ogarcia-market <60816648+ogarcia-dev@users.noreply.github.com>
2025-02-28 13:05:03 -0500

    Update InfoBannerPP.js

--- components/productosParticipantes/InfoBannerPP.js
+++ components/productosParticipantes/InfoBannerPP.js
@@ -1,79 +1,52 @@
 import SvgPP from "./SvgPP";
 
 const InfoBannerPP = () => {
-  const segments = [
-    {
-      url: "/assets/dashboard/DC.webp",
-      text: "Creative Cloud",
-      subtext: "CCT  |  CCT Pro  |  CCE Ed 4 | Adobe Express",
-    },
-    {
-      url: "/assets/dashboard/cc.webp",
-      text: "Document Cloud",
-      subtext: "Acrobat Pro  |  Acrobat Sign  |  Acrobat Premium  |  Acrobat AI Assistant",
-    },
-  ];
 
-  const secuency = [0, 1, 1, 0, 1, 1, 1, 1];
+    return (
+        <div className="flex flex-col gap-3 -mb-12">
+            <div className="grid grid-cols-7 mt-6">
+                <div className="grid grid-cols-1 lg:col-span-6 col-span-7 gap-6 w-full lg:px-6">
+                    <div className="w-full p-1 h-1/2 flex justify-center items-center rounded-lg">
+                        <div className="flex items-center ml-12 mr-12">
+                            <figure className="w-[50px]">
+                                <img src="https://res.cloudinary.com/dechrcyu3/image/upload/v1740763789/Modo_de_aislamiento_onct8d.png" />
+                            </figure>
+                        </div>
 
-  return (
-    <div className="flex flex-col gap-3">
-      <div className="grid grid-cols-7">
-        <div className="col-span-7 flex justify-center lg:col-span-1">
-          <SvgPP />
-        </div>
-        <div className="grid grid-cols-2 lg:col-span-6 col-span-7 gap-6 w-full lg:px-6">
-          {segments.map((item) => (
-            <div>
-              <div className="w-full bg-[#F4F4F4] p-1 h-1/2 flex justify-center items-center rounded-lg">
-                <div className="flex items-center">
-                  <figure className="w-[50px]">
-                    <img src={item.url} />
-                  </figure>
-                  
-                  <p className="xl:!text-base font-semibold">{item.text}</p>
+                        <div className="flex items-center ml-12 mr-12">
+                            <figure className="w-[150px]">
+                                <img src="https://res.cloudinary.com/dechrcyu3/image/upload/v1740763789/Group_1000006047_rk81oi.png" />
+                            </figure>
+                        </div>
+
+                        <div className="flex items-center ml-12 mr-12">
+                            <figure className="w-[50px]">
+                                <img src="https://res.cloudinary.com/dechrcyu3/image/upload/v1740763789/Modo_de_aislamiento_1_jkkff9.png" />
+                            </figure>
+                        </div>
+
+                        <div className="flex flex-col items-center gap-2 mt-8">
+                            <div className="w-[3px] h-10 bg-black" />
+                            <p className="pl-5 w-[100px] xl:text-base font-semibold">All apps</p>
+                        </div>
+
+
+                        <div className="flex items-center ml-12 mr-12">
+                            <figure className="w-[50px]">
+                                <img src="https://res.cloudinary.com/dechrcyu3/image/upload/v1740763790/Group_1000006048_grxfk8.png" />
+                            </figure>
+                        </div>
+
+                        <div className="flex items-center ml-12 mr-12">
+                            <figure className="w-[50px]">
+                                <img src="https://res.cloudinary.com/dechrcyu3/image/upload/v1740763789/Adobe_Express_logo_1_v2ayo2.png" />
+                            </figure>
+                        </div>
+                    </div>
                 </div>
-              </div>
-              <div className="h-1/2 w-full flex justify-center items-center">
-                <p className="!text-xs font-semibold">{item.subtext}</p>
-              </div>
             </div>
-          ))}
         </div>
-      </div>
-      <div className="grid grid-cols-7">
-        <div className="flex flex-col w-full items-center gap-6 p-6 bg-[#E0E0E0] rounded-lg [&_p]:!text-xs [&_p]:font-semibold lg:col-span-1 col-span-2">
-          <p>DC</p>
-          <hr className="w-full" />
-          <p>Teams</p>
-          <hr className="w-full" />
-          <p>Enterprise</p>
-          <hr className="w-full" />
-          <p>Education</p>
-        </div>
-        <div className="grid grid-cols-2 lg:col-span-6 col-span-5 place-items-center px-6 tableProducts">
-          {secuency.map((item) => (
-            <div className="w-full flex justify-center items-center border-b h-full">
-              {item === 1 && (
-                <svg
-                  width="20"
-                  height="20"
-                  viewBox="0 0 20 20"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    d="M10 0C4.48606 0 0 4.48606 0 10C0 15.5139 4.48606 20 10 20C15.5139 20 20 15.5139 20 10C20 4.48606 15.5139 0 10 0ZM8.17308 15.0183L4.29808 10.712L5.44183 9.68269L8.13414 12.674L14.5192 5.07019L15.699 6.05769L8.17308 15.0183Z"
-                    fill="#EB1000"
-                  />
-                </svg>
-              )}
-            </div>
-          ))}
-        </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default InfoBannerPP;
