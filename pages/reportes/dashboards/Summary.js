15f5be3a059489204ba80b8672ffc64d84d1dc87
ogarcia-market <60816648+ogarcia-dev@users.noreply.github.com>
2025-04-03 17:35:26 -0500

    Update Summary.js

--- pages/reportes/dashboards/Summary.js
+++ pages/reportes/dashboards/Summary.js
@@ -83,24 +83,24 @@ const Summary = () => {
           }`}
           onClick={() => setContent(t("SalesYoy"))}
         /> */}
-                <ButtonBgOut
+                {/* <ButtonBgOut
                     title={t("Reportes.digipoints_performance")}
                     styles={`${content === "DigipoinstPerformance"
                             ? "bg-red-100"
                             : "hover:bg-red-100 hover:!text-red-500 hover:!text-sm"
                         }`}
                     onClick={() => setContent(t("DigipoinstPerformance"))}
-                />
+                /> */}
             </div>
             <div className={`${content === "SalesYtd" ? "block" : "hidden"}`}>
                 <SalesYtd />
             </div>
-            <div
+            {/* <div
                 className={`${content === "DigipoinstPerformance" ? "block" : "hidden"
                     }`}
             >
                 <DigipoinstPerformance />
-            </div>
+            </div> */}
         </div>
     );
 };
